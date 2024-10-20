65eb4240b115778919aad67cfbed3cfdf52b1cd8
Kashif Mustari <kashifmustari@gmail.com>
2024-10-20 22:55:59 +0600

    some change and resources

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your food</h2>
+        <p>It's the food and groceries you love, delivered</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
