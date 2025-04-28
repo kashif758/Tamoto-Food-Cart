20cd209fba9ca7effd6b471d501233b3d8fc9085
Kashif Mustari <kashifmustari@example.com>
2025-04-28 19:37:22 +0600

    Initial commit

--- src/components/Navbar/Header/Header.jsx
+++ src/components/Navbar/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className='header'>
       <div className="header-contents">
-        <h2>Order your food</h2>
+        <h2>Order your favorite food here</h2>
         <p>It's the food and groceries you love, delivered</p>
         <button>View Menu</button>
       </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../components/Navbar/Header/Header'
+import ExploreMenue from '../../components/ExploreMenue/ExploreMenue'
+import { useState } from 'react'
+
 const Home = () => {
+
+    const [category,setCategory] = useState("All");
+
   return (
     <div>
       <Header/>
+      <ExploreMenue category={category}setCategory={setCategory}/>
     </div>
   )
 }
