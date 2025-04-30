e81a28426b8ba847455d69774fd6b9426c1d1452
Kashif Mustari <kashifmustari@example.com>
2025-04-30 19:09:29 +0600

    Some new lines

--- src/components/ExploreMenue/ExploreMenue.jsx
+++ src/components/ExploreMenue/ExploreMenue.jsx
@@ -2,24 +2,29 @@ import React from 'react'
 import './ExploreMenue.css'
 import { menu_list } from '../../assets/assets'
 
-const ExploreMenue = ({category,setCategory}) => {
+const ExploreMenue = ({ category, setCategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore our menu</h1>
-        <p className='explore-menu-text'>Choos fore a diverse menu featuring a delectable array of dishes.Our mission is satify you cravings and elevate your dining experiene,one delicious meal at a time.</p>
-        <div className="explore-menu-list">
-            {menu_list.map((item,index)=>{
-                return(
-                    <div onClick={()=>setCategory(prev===item.menu_name?"ALL":item.menu_name)} key={index} className='explore-menu-list-item'>
-                    <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
-                    <p>{item.menu_name}</p>
-                </div>
-                )
-            })}
-        </div>
-        <hr/>
+      <h1>Explore our menu</h1>
+      <p className='explore-menu-text'>
+        Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => (
+          <div
+            onClick={() => setCategory(prev => prev === item.menu_name ? "ALL" : item.menu_name)}
+            key={index}
+            className='explore-menu-list-item'
+          >
+            <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt={item.menu_name} />
+            <p>{item.menu_name}</p>
+          </div>
+        ))}
+      </div>
+      <hr />
     </div>
   )
 }
 
 export default ExploreMenue
+
